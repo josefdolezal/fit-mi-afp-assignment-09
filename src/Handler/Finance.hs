210a939ee654f1d1f737f71d663ff1694203efac
Josef Dolezal <pepik.ml@gmail.com>
2018-05-06 15:17:32 +0200

    Add record creation form

--- src/Handler/Finance.hs
+++ src/Handler/Finance.hs
@@ -6,13 +6,25 @@
 module Handler.Finance where
 
 import Import
+import Yesod.Form.Bootstrap3
+
+financeRecordForm :: AForm Handler FinanceRecord
+financeRecordForm = FinanceRecord
+    <$> areq textField (bfs ("Description" :: Text)) Nothing
+    <*> areq doubleField (bfs ("Amount" :: Text)) Nothing
+    <*> areq intField (bfs ("Timestamp" :: Text)) Nothing
 
 getFinanceR :: Handler Html
-getFinanceR = defaultLayout $ do
-    let records = [] :: [FinanceRecord]
-    $(widgetFile "homepage")
+getFinanceR = do
+    (widget, _) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm financeRecordForm
+    defaultLayout $ do
+        $(widgetFile "finance/new")
 
 postFinanceR :: Handler Html
-postFinanceR = defaultLayout $ do
-    let records = [] :: [FinanceRecord]
-    $(widgetFile "homepage")
+postFinanceR = do
+    ((res, widget), _) <- runFormPost $ renderBootstrap3 BootstrapBasicForm financeRecordForm
+    case res of
+        FormSuccess record -> do
+            _ <- runDB $ insert record
+            redirect HomeR
+        _ -> defaultLayout $(widgetFile "finance/new")
