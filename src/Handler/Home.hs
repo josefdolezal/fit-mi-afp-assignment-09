baf823bddfc11d88d4b6ab430b6a1510c7baadd9
Josef Dolezal <pepik.ml@gmail.com>
2018-05-06 16:55:42 +0200

    Show all records on home, fix inline creation form

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -6,11 +6,28 @@
 module Handler.Home where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
+import Yesod.Form.Bootstrap3
 import Text.Julius (RawJS (..))
 
 getHomeR :: Handler Html
 getHomeR = do
-    let records = [] :: [FinanceRecord]
-    defaultLayout $ do
-        $(widgetFile "homepage")
+    records <- runDB $ selectList [] [Desc FinanceRecordId]
+    (widget, _) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm inlineFinanceRecordForm
+    defaultLayout $(widgetFile "homepage")
+
+postHomeR :: Handler Html
+postHomeR = do
+    records <- runDB $ selectList [] [Desc FinanceRecordId]
+    ((res, widget), _) <- runFormPost $ renderBootstrap3 BootstrapBasicForm inlineFinanceRecordForm
+    case res of
+        FormSuccess record -> do
+            _ <- runDB $ insert record
+            redirect HomeR
+        _ -> defaultLayout $(widgetFile "homepage")
+
+inlineFinanceRecordForm :: AForm Handler FinanceRecord
+inlineFinanceRecordForm = FinanceRecord
+    <$> areq textField (attrs "Description") Nothing
+    <*> areq doubleField (attrs "Amount") Nothing
+    <*> areq intField (attrs "Timestamp") Nothing
+    where attrs placeholder = withPlaceholder placeholder (bfs ("" :: Text))
